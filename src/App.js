5e5c1f0f994c9996d9e332f8d5039aa11aea16a2
msoni6844 <68237348+msoni6844@users.noreply.github.com>
2022-08-02 15:40:24 +0530

    limit change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PokemonThumbnail from "./Components/PokemonThumbnail";
 
 function App() {
   const [allPokemons,setAllPokemons] = useState([]);
-  const [loadPoke,setLoadPoke] = useState('https://pokeapi.co/api/v2/pokemon?limit=14');
+  const [loadPoke,setLoadPoke] = useState('https://pokeapi.co/api/v2/pokemon?limit=21');
   const getAllPokemons = async () =>{
     const res = await fetch(loadPoke)
     const data = await res.json()
